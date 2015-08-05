886dd62c0d1d9614198243d3732a300feb3fec53
reynoldsjay <jayrey000@gmail.com>
2015-08-05 18:53:22 -0400

    Non-facebook login implemented.

--- QueueUp/LoginViewController.m
+++ QueueUp/LoginViewController.m
@@ -12,17 +12,29 @@
 #import "ServerAPI.h"
 #import "Config.h"
 
-@implementation LoginViewController
+@interface LoginViewController ()
+
+@property IBOutlet UITextField *emailField;
+@property IBOutlet UITextField *passwordField;
+@property IBOutlet UITextField *nameField;
+
+
+@end
+
+@implementation LoginViewController {
+    ServerAPI *api;
+}
 
 
 - (void) viewDidLoad {
     // fb login button
     FBSDKLoginButton *loginButton = [[FBSDKLoginButton alloc] init];
     loginButton.readPermissions = @[@"public_profile", @"email", @"user_friends"];
-    loginButton.center = self.view.center;
+    loginButton.center = CGPointMake(self.view.center.x, 80);
     [self.view addSubview:loginButton];
     // notify view on callbak
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(appDidBecomeActive:) name:UIApplicationDidBecomeActiveNotification object:nil];
+    api = [ServerAPI getInstance];
 
 }
 
@@ -33,7 +45,7 @@
         // send fb access token
         NSLog(@"User logged in.");
         NSString* accessString = [[NSString alloc] initWithFormat:@"{\"facebook_access_token\" : \"%@\"}", [FBSDKAccessToken currentAccessToken].tokenString];
-        ServerAPI *api = [ServerAPI getInstance];
+        
         id json = [api parseJson:accessString];
         NSString *client = [api postData:json toURL:(@"/api/v2/auth/login")];
         // store client id
@@ -49,6 +61,46 @@
         //
     }
     
+}
+
+- (IBAction)registerClick:(id)sender {
+    NSString* regString = [[NSString alloc] initWithFormat:@"{\"email\" : \"%@\", \"name\" : \"%@\", \"password\" : \"%@\"}", self.emailField.text, self.nameField.text, self.passwordField.text];
+    
+    id json = [api parseJson:regString];
+    NSString *client = [api postData:json toURL:(@"/api/v2/auth/register")];
+    // store client id
+    NSString *theID = ((NSDictionary*)[api parseJson:client])[@"user_id"];
+    
+    
+    NSString *token = ((NSDictionary*)[api parseJson:client])[@"client_token"];
+    NSString *combine = [[NSString alloc] initWithFormat:@"{\"user_id\":\"%@\", \"client_token\":\"%@\"}", theID, token];
+    api.idAndToken = [api parseJson:combine];
+    [self performSegueWithIdentifier:@"login" sender:self];
+    
+}
+
+- (IBAction)loginClick:(id)sender {
+    NSString* logString = [[NSString alloc] initWithFormat:@"{\"email\" : \"%@\", \"password\" : \"%@\"}", self.emailField.text, self.passwordField.text];
+    id json = [api parseJson:logString];
+    NSString *client = [api postData:json toURL:(@"/api/v2/auth/login")];
+    if (((NSDictionary*)[api parseJson:client])[@"user_id"]) {
+        // store client id
+        NSString *theID = ((NSDictionary*)[api parseJson:client])[@"user_id"];
+        
+        
+        NSString *token = ((NSDictionary*)[api parseJson:client])[@"client_token"];
+        NSString *combine = [[NSString alloc] initWithFormat:@"{\"user_id\":\"%@\", \"client_token\":\"%@\"}", theID, token];
+        api.idAndToken = [api parseJson:combine];
+        [self performSegueWithIdentifier:@"login" sender:self];
+    } else {
+        NSLog(@"wrong password");
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Wrong email/password combination."
+                                                        message:@""
+                                                       delegate:nil
+                                              cancelButtonTitle:@"OK"
+                                              otherButtonTitles:nil];
+        [alert show];
+    }
 }
 
 @end
